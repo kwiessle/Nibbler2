ad02b58a21799f5b223d16ff0b01652d2712ff97
kwiessle <kiefer@iMac-de-Kiefer.local>
2018-03-07 00:15:55 +0100

    lint

--- Timer/Timer.cpp
+++ Timer/Timer.cpp
@@ -1,22 +1,19 @@
 #include "Timer.hpp"
 
-Timer::Timer() {
-	this->_refTimer = _getMilliCount();
-	this->_newTimer = _getMilliCount();
-	this->_diffTime = 1000;
+Timer::Timer(void) { return; }
+
+Timer::Timer(int diff):
+  _diffTime(diff)
+{
+	   this->_refTimer = _getMilliCount();
+     return;
 }
 
-Timer::Timer(int diff): _diffTime(diff){
-	this->_refTimer = _getMilliCount();
-}
-
-
-Timer::~Timer() {
-}
+Timer::~Timer() { return; }
 
 
 
-int 		Timer::_getMilliCount() {
+int 		Timer::_getMilliCount(void) {
 	timeb tb;
 	ftime(&tb);
 	int nCount = tb.millitm + tb.time * 1000;
@@ -24,7 +21,7 @@ int 		Timer::_getMilliCount() {
 }
 
 
-bool		Timer::update() {
+bool		Timer::update(void) {
 	this->_newTimer = _getMilliCount();
 	if (difftime(_newTimer, _refTimer) > _diffTime) {
 		this->_refTimer = _getMilliCount();
